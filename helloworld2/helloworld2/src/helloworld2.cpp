1797d555599568aad11da860f99076aa274aeede
Никита <@DESKTOP-BOTE313>
2019-10-04 17:19:04 +0300

    helloworld2 project added

---
+++ helloworld2/helloworld2/src/helloworld2.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : helloworld2.cpp
+// Author      : NLincon
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
