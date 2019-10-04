72db482df07dad44d9b2902e1d71f642fc342111
Никита <@DESKTOP-BOTE313>
2019-10-04 21:57:26 +0300

    Six different cycles added

--- helloworld2/helloworld2/src/helloworld2.cpp
+++ helloworld2/helloworld2/src/helloworld2.cpp
@@ -10,6 +10,72 @@
 using namespace std;
 
 int main() {
+	// Initialization
+	int i = 1;
+	do {
+		// Body
+		cout << i << endl;
+	// Condition
+	} while (i++ <= 10);
+
+
+	// Initialization
+	int i = 1;
+	do {
+		// Body
+		cout << i << endl;
+	// Condition
+	} while (!(i++ <= 10));
+
+
+	// Initialization
+	i = 10;
+	// Condition
+	while (i-- >= 1) {
+		// Body
+		cout << i << endl;
+	}
+
+
+	// Initialization
+	i = 10;
+	// Condition
+	while (!(i-- >= 1)) {
+		// Body
+		cout << i << endl;
+	}
+
+
+	for (int j = 1; j <= 10; j++) {
+		cout << j << endl;
+	}
+
+
+	for (int s = 1; true;s++) cout << s << endl;
+
+	// Initialization
+	i = 10;
+	// Condition
+	c1: {
+		// Body
+		cout << i << endl;
+	}
+	if (i-- >= 1) goto c2;
+	else
+		goto c1;
+	c2: cout << "Hello World" << endl;
+
+	// Initialization
+	i = 10;
+	// Condition
+	c3: {
+		// Body
+		cout << i << endl;
+	}
+	if (i-- < 1) goto c4;
+		goto c3;
+	c4: cout << "Hello World" << endl;
+
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
